530bc36401a95f66c2c2d9e23b33d9832190053d
akinobufujii <aki.develop@gmail.com>
2020-07-13 22:55:35 +0900

    コンパイルエラー修正、サンプルのconfig.json追加

--- main.go
+++ main.go
@@ -22,30 +22,29 @@ func LoadConfig(filename string) (Config, error) {
 	if err != nil {
 		return Config{}, err
 	}
-​
+
 	config := Config{}
 	err = json.Unmarshal(raw, &config)
 	if err != nil {
 		return Config{}, err
 	}
-​
+
 	return config, nil
 }
-​
+
 // generateNinjaFile build.ninjaジェネレータ
 func generateNinjaFile(compileCommands []compiledb.CompileCommand, config Config) {
-​
 	setting := ""
-​
+
 	setting += "rule tidy\n"
 	setting += "    description = \"tidy source code $in\"\n"
 	setting += "    command = " + config.Command + "\n\n"
-​
+
 	for _, command := range compileCommands {
 		if len(config.RootDirectory) > 0 {
 			command.File = strings.Replace(command.File, config.RootDirectory, "", -1)
 		}
-​
+
 		// 除外する単語が含まれているかを調査する
 		isOutput := true
 		for _, word := range config.IgnoreWords {
@@ -54,20 +53,20 @@ func generateNinjaFile(compileCommands []compiledb.CompileCommand, config Config
 				break
 			}
 		}
-​
+
 		if !isOutput {
 			continue
 		}
-​
+
 		output := command.File
 		output = strings.Replace(output, "/", "-", -1)
 		output = strings.Replace(output, ".cpp", "", -1)
 		setting += fmt.Sprintf("build %v: tidy %v\n", output, command.File)
 	}
-​
+
 	ioutil.WriteFile("build.ninja", []byte(setting), 0755)
 }
-​
+
 func main() {
 	// 設定読み込み
 	config, err := LoadConfig("config.json")
@@ -75,14 +74,14 @@ func main() {
 		fmt.Fprintln(os.Stderr, "load fail config.json")
 		os.Exit(1)
 	}
-​
+
 	// コンパイルDB読み込み
 	compileCommands, err := compiledb.LoadCompileDB("compile_commands.json")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "load fail compile_commadns.json")
 		os.Exit(1)
 	}
-​
+
 	// build.ninjaファイル作成
 	generateNinjaFile(compileCommands, config)
 }
